2a64c6fecf83bad4f4a7a8d618277cc7b9af44b6
jlejiaro <jlejiaro@everis.com>
2021-11-25 09:50:03 -0600

    Este si jala (creo)

--- main/src/main/java/com/example/main/main.java
+++ main/src/main/java/com/example/main/main.java
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-package com.example.main;public class main {
-=======
 package com.example.main;
 
 import com.example.bank.entity.Cliente;
@@ -9,5 +6,4 @@ public class main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
     }
->>>>>>> 2e53aa603c8a34edf15dbacb1091fea5629e2864
 }
